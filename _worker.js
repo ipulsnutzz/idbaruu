5ae2e25e70c0ba0eb121160743ac5ebeac1aaccf
Mr.A_S <36959705+haijuga7@users.noreply.github.com>
2023-12-20 18:54:44 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,4 @@
-// <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
+s// <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
 // @ts-ignore
 import { connect } from 'cloudflare:sockets';
 
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '88148567-2efb-4ce1-96b3-6aa4616cf1d3';
 
-const proxyIPs = ['163.181.19.152'];
+const proxyIPs = ['8.215.45.124'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
