a7b48641a6330b2096f2ab7f917ade088abfd4c1
ipulsnutzz <81121279+ipulsnutzz@users.noreply.github.com>
2025-02-04 02:57:43 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ const proxyIPs = ['35.219.50.99'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://cloudflare-dns.com/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables (optional) deprecated, please use planetscale.com instead
 
